faf53531c6851c7952432849a87b6f70edb10854
fen-dada <1823215739@qq.com>
2024-04-27 17:04:15 +0800

    update

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -17,11 +17,11 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!(is_even(4), "true");
+        assert!(is_even(4));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!(is_even(5), "false");
+        assert!(!is_even(7));
     }
 }
